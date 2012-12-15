ca5e1e251391f1a2f4894436a0a7551d1d359f97
William Becker <wbecker+hiveshare@gmail.com>
2012-12-15 13:09:05 +0000

    initial version

---
+++ tests/hiveshare-object-test.js
@@ -0,0 +1,16 @@
+var vows = require("vows");
+var assert = require("assert");
+var HiveShareObject = require("../lib/hiveshare-object");
+var suite = vows.describe('hiveshare-object');
+suite.addBatch({
+  "Types": {
+    "can be added": function () {
+      var o = new HiveShareObject();
+      o.addType({id: 1});
+      var types = o.getTypes();
+      assert.equal(types.length, 1);
+      assert.equal(types[0].id, 1);
+    }
+  }
+});
+suite.export(module);
