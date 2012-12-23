7fdb069a7f80eda4b6c4dc17a5c77428aea9f14d
William Becker <wbecker+hiveshare@gmail.com>
2012-12-23 13:01:00 +0000

    add hiveshare type

--- hiveshare-datamodel.js
+++ hiveshare-datamodel.js
@@ -1,3 +1,4 @@
 module.exports = {};
 module.exports.HiveShareObject = require("./lib/hiveshare-object");
+module.exports.HiveShareType = require("./lib/hiveshare-type");
 module.exports.Query = require("./lib/query");
