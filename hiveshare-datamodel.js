f49b36a4d93b736bc5a6e6582ac051926ff14eaa
William Becker <wbecker+hiveshare@gmail.com>
2012-12-25 18:30:58 +0000

    Add hard coded type ids

--- hiveshare-datamodel.js
+++ hiveshare-datamodel.js
@@ -3,3 +3,6 @@ module.exports.HiveShareObject = require("./lib/hiveshare-object");
 module.exports.HiveShareType = require("./lib/hiveshare-type");
 module.exports.HiveShareTag = require("./lib/hiveshare-tag");
 module.exports.Query = require("./lib/query");
+module.exports.DEFAULT_TYPE_ID = 1;
+module.exports.TYPE_TYPE_ID = 2;
+module.exports.TAG_TYPE_ID = 3;
