1be0bb2412b20cc4e0e2d55733fe2834879d188d
William Becker <wbecker+hiveshare@gmail.com>
2012-12-25 15:24:16 +0000

    add tags

--- hiveshare-datamodel.js
+++ hiveshare-datamodel.js
@@ -1,4 +1,5 @@
 module.exports = {};
 module.exports.HiveShareObject = require("./lib/hiveshare-object");
 module.exports.HiveShareType = require("./lib/hiveshare-type");
+module.exports.HiveShareTag = require("./lib/hiveshare-tag");
 module.exports.Query = require("./lib/query");
